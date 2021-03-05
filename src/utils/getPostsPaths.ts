ea4cd4dc3c8abd3fc56ccb7b7f3d86dab45c36bb
Marcin Maslanka <maslankam92@gmail.com>
2021-03-05 16:35:53 +0100

    test

--- src/utils/getPostsPaths.ts
+++ src/utils/getPostsPaths.ts
@@ -5,7 +5,14 @@ import getConfig from "next/config";
 const { serverRuntimeConfig } = getConfig();
 
 const getPostsPaths = async () => {
-  const algorithmsDir = join(process.cwd(), "public", "data", "algorithms");
+  console.log("dirTree", JSON.stringify(dirTree(".")));
+  console.log("dirTree2", JSON.stringify(dirTree("./")));
+  console.log("dirTree3", JSON.stringify(dirTree(process.cwd())));
+  console.log("dirTree4", JSON.stringify(dirTree(join(process.cwd()))));
+  console.log("dirTree5", JSON.stringify(dirTree(resolve(process.cwd()))));
+  console.log("dirTree6", JSON.stringify(dirTree(serverRuntimeConfig.PROJECT_ROOT)));
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // const algorithmsDir = join(process.cwd(), "public", "data", "algorithms");
   const dataStructuresDir = join(
     process.cwd(),
     "public",
@@ -13,48 +20,49 @@ const getPostsPaths = async () => {
     "data-structures",
   );
 
-  const ddddd = join(serverRuntimeConfig.PROJECT_ROOT);
-  const ddddd2 = join(
-    serverRuntimeConfig.PROJECT_ROOT,
-    "./public/data/algorithms/",
-  );
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // const ddddd = join(serverRuntimeConfig.PROJECT_ROOT);
+  // const ddddd2 = join(
+  //   serverRuntimeConfig.PROJECT_ROOT,
+  //   "./public/data/algorithms/",
+  // );
 
-  console.log("ddddd", ddddd);
-  console.log("ddddd2", ddddd2);
+  // console.log("ddddd", ddddd);
+  // console.log("ddddd2", ddddd2);
 
   const asd = resolve("./public", "data", "algorithms");
-  console.log("??????????? ", asd);
+  // console.log("??????????? ", asd);
 
-  console.log("@@@ algorithmsDir", algorithmsDir);
-  console.log("@@@ dataStructuresDir", dataStructuresDir);
+  // console.log("@@@ algorithmsDir", algorithmsDir);
+  // console.log("@@@ dataStructuresDir", dataStructuresDir);
 
-  const tree = dirTree(asd, { extensions: /\.md/ });
-  const tree2 = dirTree(ddddd2, { extensions: /\.md/ });
+  // const tree = dirTree(asd, { extensions: /\.md/ });
+  // const tree2 = dirTree(ddddd2, { extensions: /\.md/ });
 
-  console.log("????? tree", JSON.stringify(tree, null, 2));
-  console.log("????? tree2", JSON.stringify(tree2, null, 2));
+  // console.log("????? tree", JSON.stringify(tree, null, 2));
+  // console.log("????? tree2", JSON.stringify(tree2, null, 2));
 
   const algorithmsReadmeFiles = await glob(asd + "/**/README.md");
-  const algorithmsReadmeFiles2 = await glob(ddddd2 + "/**/README.md");
-  console.log("@@@ algorithmsReadmeFiles", algorithmsReadmeFiles);
-  console.log("@@@ algorithmsReadmeFiles2", algorithmsReadmeFiles2);
+  // const algorithmsReadmeFiles2 = await glob(ddddd2 + "/**/README.md");
+  // console.log("@@@ algorithmsReadmeFiles", algorithmsReadmeFiles);
+  // console.log("@@@ algorithmsReadmeFiles2", algorithmsReadmeFiles2);
   const dataStructuresReadmeFiles = await glob(
     dataStructuresDir + "/**/README.md",
   );
-  console.log("@@@ dataStructuresReadmeFiles", algorithmsReadmeFiles);
+  // console.log("@@@ dataStructuresReadmeFiles", algorithmsReadmeFiles);
 
   const mappedAlgorithmsReadmeFiles = algorithmsReadmeFiles.map((fullPath) => {
-    console.log("@@@ fullPath", fullPath);
+    // console.log("@@@ fullPath", fullPath);
     const index = fullPath.indexOf("algorithms");
     const short = fullPath.slice(index);
     const splitted = short.split("/");
     const slug = splitted.slice(0, splitted.length - 1);
-    console.log("@@@ slug", slug);
+    // console.log("@@@ slug", slug);
 
     return { params: { slug } };
   });
 
-  console.log("@@@ mappedAlgorithmsReadmeFiles", mappedAlgorithmsReadmeFiles);
+  // console.log("@@@ mappedAlgorithmsReadmeFiles", mappedAlgorithmsReadmeFiles);
 
   const mappedDataStructuresReadmeFiles = dataStructuresReadmeFiles.map(
     (fullPath) => {
