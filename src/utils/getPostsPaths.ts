beca389c912d0c5de8d1b225e050c53fce3f8ac5
Marcin Maslanka <maslankam92@gmail.com>
2021-03-05 16:50:34 +0100

    test

--- src/utils/getPostsPaths.ts
+++ src/utils/getPostsPaths.ts
@@ -1,37 +1,37 @@
-import { join, resolve } from "path";
+import { join } from "path";
 import glob from "glob-promise";
 import dirTree from "directory-tree";
-import getConfig from "next/config";
-const { serverRuntimeConfig } = getConfig();
+// import getConfig from "next/config";
+// const { serverRuntimeConfig } = getConfig();
 
 const getPostsPaths = async () => {
   console.log("dirTree", JSON.stringify(dirTree("./public"), null, 2));
-  console.log("dirTree1", JSON.stringify(dirTree("./src/data"), null, 2));
-  console.log(
-    "dirTree1",
-    JSON.stringify(await glob("./public/**/README.md"), null, 2),
-  );
-  console.log("dirTree2", JSON.stringify(dirTree("./public"), null, 2));
-  console.log(
-    "dirTree3",
-    JSON.stringify(dirTree(process.cwd() + "/public"), null, 2),
-  );
-  console.log(
-    "dirTree4",
-    JSON.stringify(dirTree(join(process.cwd(), "/public")), null, 2),
-  );
-  console.log(
-    "dirTree5",
-    JSON.stringify(dirTree(resolve(process.cwd(), "/public")), null, 2),
-  );
-  console.log(
-    "dirTree6",
-    JSON.stringify(
-      dirTree(serverRuntimeConfig.PROJECT_ROOT + "/public"),
-      null,
-      2,
-    ),
-  );
+  // console.log("dirTree1", JSON.stringify(dirTree("./src/data"), null, 2));
+  // console.log(
+  //   "dirTree1",
+  //   JSON.stringify(await glob("./public/**/README.md"), null, 2),
+  // );
+  // console.log("dirTree2", JSON.stringify(dirTree("./public"), null, 2));
+  // console.log(
+  //   "dirTree3",
+  //   JSON.stringify(dirTree(process.cwd() + "/public"), null, 2),
+  // );
+  // console.log(
+  //   "dirTree4",
+  //   JSON.stringify(dirTree(join(process.cwd(), "/public")), null, 2),
+  // );
+  // console.log(
+  //   "dirTree5",
+  //   JSON.stringify(dirTree(resolve(process.cwd(), "/public")), null, 2),
+  // );
+  // console.log(
+  //   "dirTree6",
+  //   JSON.stringify(
+  //     dirTree(serverRuntimeConfig.PROJECT_ROOT + "/public"),
+  //     null,
+  //     2,
+  //   ),
+  // );
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   // const algorithmsDir = join(process.cwd(), "public", "data", "algorithms");
   const dataStructuresDir = join(
@@ -51,7 +51,7 @@ const getPostsPaths = async () => {
   // console.log("ddddd", ddddd);
   // console.log("ddddd2", ddddd2);
 
-  const asd = resolve("./public", "data", "algorithms");
+  // const asd = resolve("./public", "data", "algorithms");
   // console.log("??????????? ", asd);
 
   // console.log("@@@ algorithmsDir", algorithmsDir);
@@ -63,7 +63,9 @@ const getPostsPaths = async () => {
   // console.log("????? tree", JSON.stringify(tree, null, 2));
   // console.log("????? tree2", JSON.stringify(tree2, null, 2));
 
-  const algorithmsReadmeFiles = await glob(asd + "/**/README.md");
+  const algorithmsReadmeFiles = await glob(
+    ".public/data/algorithms/**/README.md",
+  );
   // const algorithmsReadmeFiles2 = await glob(ddddd2 + "/**/README.md");
   // console.log("@@@ algorithmsReadmeFiles", algorithmsReadmeFiles);
   // console.log("@@@ algorithmsReadmeFiles2", algorithmsReadmeFiles2);
